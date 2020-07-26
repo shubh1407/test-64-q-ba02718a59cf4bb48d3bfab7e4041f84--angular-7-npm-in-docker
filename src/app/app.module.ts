3455ec88dc221ef0e2f8bbc04993a2e9ffbc2d9d
shubh1407 <shubhamagarwal1407@gmail.com>
2020-07-26 20:54:56 +0530

    Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,19 +21,6 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     FormsModule,
     ModalModule.forRoot(),
     
-=======
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
->>>>>>> 0dddc4696c126d0c783db26cecf820950434b31e
   ],
   providers: [],
   bootstrap: [AppComponent]
